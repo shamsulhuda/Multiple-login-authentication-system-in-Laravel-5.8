4f40e8cc1bff48ed42c2f2766bdec8cea3005037
shamsulhuda <shamsulhuda310@gmail.com>
2019-09-01 16:04:31 +0600

    login-and-registration

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+    public function __construct(){
+        $this->middleware('guest:admin');
+    }
+
+    public function showLoginForm(){
+        return view('auth.admin-login');
+    }
+
+    public function login(Request $request){
+        // steps we need to follow
+        // step 1 validate the form data
+        // step 2 attempt to log the user in
+        // step 3 If successful then, redirect to their intended location
+        // step 4 If not then, redirect back to the login form
+
+        //step 1
+        $this->validate($request,[
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        //step 2
+        // Auth::attempt($credentials, $remember);
+        $loginData = Auth::guard('admin')->attempt(['email'=>$request->email, 'password'=>$request->password], $request->remember);
+        
+        //step 3
+        if ($loginData) {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        //step 4
+        return redirect()->back()->withInput($request->only('email', 'remember'));
+        
+    }
+}
