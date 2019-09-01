03fcb27a8737a4bf57725244c05e5b00a4e6fc0a
shamsulhuda <shamsulhuda310@gmail.com>
2019-09-01 17:25:05 +0600

    login-specification

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +47,37 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof AuthenticationException){
+
+            $guard = array_get($exception->guards(), 0);
+            switch($guard){
+                case 'admin':
+                    return redirect(route('admin.login'));
+                    break;
+                default:
+                    return redirect(route('login'));
+                    break;
+            }
+        }
         return parent::render($request, $exception);
     }
-}
+
+//     protected function unauthenticated($request, AuthenticationException $exception){
+//         if ($request->expectJson()) {
+//             return response()->json(['error' => 'Unauthenticated.'], 401);
+//         }
+
+//         $guard = array_get($exception->guard(), 0);
+
+//         switch ($guard) {
+//             case 'admin':
+//                 $login = 'admin.login';
+//                 break;
+            
+//             default:
+//                 $login = 'login';
+//                 break;
+//         }
+//         return redirect()->guest(route($login));
+//     }
+ }
