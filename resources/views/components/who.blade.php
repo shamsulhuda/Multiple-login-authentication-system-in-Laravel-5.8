ad84f14dd06016e8ab76c4d53cd96ce3bb538934
shamsulhuda <shamsulhuda310@gmail.com>
2019-09-01 17:52:01 +0600

    Login-logut-message

---
+++ resources/views/components/who.blade.php
@@ -0,0 +1,20 @@
+@if (Auth::guard('web')->check())
+    <p class="text-success">
+        You're Logged in as a <strong>USER</strong>!
+    </p>
+@else
+    <p class="text-danger">
+        You're Logged out as a <strong>USER</strong>!
+    </p>
+@endif
+
+
+@if (Auth::guard('admin')->check())
+    <p class="text-success">
+        You're Logged in as a <strong>ADMIN</strong>!
+    </p>
+@else
+    <p class="text-danger">
+        You're Logged out as a <strong>ADMIN</strong>!
+    </p>
+@endif
