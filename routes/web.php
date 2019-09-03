908485e1bc952db8593a5d75f85fca2d1640cc1c
shamsulhuda <shamsulhuda310@gmail.com>
2019-09-03 18:10:49 +0600

    Reset password functinality done

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,12 @@ Route::prefix('/admin')->group(function(){
 
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
     Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
+
+    // Reset password routes
+    Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset')->name('admin.password.update');
+    Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
 });
 
 
